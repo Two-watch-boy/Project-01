85ebd6cfe7c4676c423723524c79e82f8e46ec25
Two-Watch-Boy <twowatchboy@gmail.com>
2016-04-12 09:39:14 -0700

    heroku push attempt 3

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect( process.env.MONGOLAB_URI ||
                       process.env.MONGOHQ_URL ||
-                      "mongodb://localhost/primary-demo" );
+                      "mongodb://heroku_qztn11jb:vlsa6p9am8os6l80dfckv9h8r7@ds023510.mlab.com:23510/heroku_qztn11jb" );
 
 var Bag = require('./bag');
 module.exports.Bag = Bag;
